7eff7dff2362d8e0ad3249174fbab3fa0958430f
Muhammad-Rizwan-Sheikh <159832035+Muhammad-Rizwan-Sheikh@users.noreply.github.com>
2024-02-27 21:39:41 +0500

    Add files via upload

---
+++ app.ts
@@ -0,0 +1,92 @@
+// let myName = "okasha";
+
+// myName = "Miss Atia";
+//  console.log(myName); 
+
+// / there are two type of inference/
+// No.1 string 
+// No.2 strong
+
+
+
+
+
+
+// Arithematic Operators
+
+
+// let n1 = 1;
+// let n2 = 2;
+// console.log(n1 + n2);
+//     1+2 //
+
+
+
+
+// console.log(10+20)
+
+
+// let num1 = 7;
+// let num2 = 8;
+// let sum = num1+num2;
+// //   7  +  8
+
+
+// console.log(sum)
+
+
+
+// let num1 = 7;
+// let num2 = 8;
+// let sum = num1-num2;
+
+
+
+// console.log(sum)
+
+
+// let num1 = 6;
+// let num2 = 3;
+
+// console.log(num1 * num2)
+
+
+// let num1 = 20;
+// let num2 = 5;
+
+// console.log(num1 % num2)
+//    20 % 5 ans = 0 //
+
+
+// let num1A = 21;
+// let num2A = 4;
+
+// console.log(num1A / num2A)
+// //   21 / 4 ans 5.25  //
+
+
+// let n = 5;
+// n = n + 6;
+// console.log(n);
+// // n + n = 11 //
+
+
+// let n = 5;
+// n += 6;
+// console.log(n);
+//  previous value ko add krdia n ki value ko 6 sa plus krdia  ans = 11 //
+
+
+
+
+// let n = 5;
+// n -= 6;
+// console.log(n);
+//  previous value ko subtract krdia n ki value ko 6 sa minus krdia  ans = -1 //
+
+// let n = 5;
+// n = 5;
+// console.log (n == 5);
+
+
+// Arithematic Operators
